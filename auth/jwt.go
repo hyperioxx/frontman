21b88d5ef82c92cc9762f31523c1ed1aad0d98b0
Christophe Collot <christophe.collot@qonto.com>
2023-04-03 17:56:32 +0200

    require Bearer prefix for token authentication

--- auth/jwt.go
+++ auth/jwt.go
@@ -2,13 +2,15 @@ package auth
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"strings"
+
+	"net/http"
 
 	"github.com/lestrrat-go/jwx/v2/jwk"
 	"github.com/lestrrat-go/jwx/v2/jws"
 	"github.com/lestrrat-go/jwx/v2/jwt"
-	"net/http"
 )
 
 type JWTValidator struct {
@@ -16,6 +18,8 @@ type JWTValidator struct {
 	audience string
 	JWKS     jwk.Set
 }
+
+const AuthTypeBearer string = "bearer"
 
 func NewJWTValidator(issuer string, audience string, jwkUrl string) (*JWTValidator, error) {
 	jwks, err := jwk.Fetch(context.Background(), jwkUrl)
@@ -33,6 +37,9 @@ func NewJWTValidator(issuer string, audience string, jwkUrl string) (*JWTValidat
 func (v JWTValidator) ValidateToken(request *http.Request) (map[string]interface{}, error) {
 	tokenString := request.Header.Get("Authorization")
 	splitToken := strings.Fields(tokenString)
+	if strings.ToLower(splitToken[0]) != AuthTypeBearer {
+		return nil, fmt.Errorf("unsupported authorization type, expected 'Bearer' %w", http.ErrNotSupported)
+	}
 	// Remove leading "Bearer "
 	token := splitToken[len(splitToken)-1]
 	result, err := jwt.Parse([]byte(token), jwt.WithKeySet(v.JWKS, jws.WithInferAlgorithmFromKey(true)))
