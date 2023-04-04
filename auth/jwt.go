0b97dd9598acec68f0d34101b92735489c957a43
Christophe Collot <christophe.collot@qonto.com>
2023-04-04 15:25:29 +0200

    refactor jwt and its tests

--- auth/jwt.go
+++ auth/jwt.go
@@ -2,12 +2,14 @@ package auth
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"strings"
 
 	"net/http"
 
+	"github.com/Frontman-Labs/frontman/config"
 	"github.com/lestrrat-go/jwx/v2/jwk"
 	"github.com/lestrrat-go/jwx/v2/jws"
 	"github.com/lestrrat-go/jwx/v2/jwt"
@@ -19,32 +21,53 @@ type JWTValidator struct {
 	JWKS     jwk.Set
 }
 
+type JWTValidatorOption func(*JWTValidator)
+
 const AuthTypeBearer string = "bearer"
 
-func NewJWTValidator(issuer string, audience string, jwkUrl string) (*JWTValidator, error) {
-	jwks, err := jwk.Fetch(context.Background(), jwkUrl)
-	if err != nil {
-		log.Printf("Error loading jwks from %s: %s", jwkUrl, err.Error())
-		return nil, err
+var (
+	ErrMissingAuthHeader   = errors.New("missing authorization header")
+	ErrBadFormatAuthHeader = errors.New("invalid format for authorization header")
+)
+
+func NewJWTValidator(cfg *config.JWTConfig, opts ...JWTValidatorOption) (*JWTValidator, error) {
+	jwks := jwk.NewSet()
+	if cfg.KeysUrl != "" {
+		keySet, err := jwk.Fetch(context.Background(), cfg.KeysUrl)
+		if err != nil {
+			log.Printf("Error loading jwks from %s: %s", cfg.KeysUrl, err.Error())
+			return nil, err
+		}
+		jwks = keySet
 	}
-	return &JWTValidator{
-		issuer:   issuer,
-		audience: audience,
+	validator := &JWTValidator{
+		issuer:   cfg.Issuer,
+		audience: cfg.Audience,
 		JWKS:     jwks,
-	}, nil
+	}
+	for _, opt := range opts {
+		opt(validator)
+	}
+	return validator, nil
 }
 
 func (v JWTValidator) ValidateToken(request *http.Request) (map[string]interface{}, error) {
 	tokenString := request.Header.Get("Authorization")
+	if len(tokenString) == 0 {
+		return nil, ErrMissingAuthHeader
+	}
 	splitToken := strings.Fields(tokenString)
+	if len(splitToken) < 2 {
+		return nil, ErrBadFormatAuthHeader
+	}
 	if strings.ToLower(splitToken[0]) != AuthTypeBearer {
 		return nil, fmt.Errorf("unsupported authorization type, expected 'Bearer' %w", http.ErrNotSupported)
 	}
-	// Remove leading "Bearer "
 	token := splitToken[len(splitToken)-1]
 	result, err := jwt.Parse([]byte(token), jwt.WithKeySet(v.JWKS, jws.WithInferAlgorithmFromKey(true)))
 	if err != nil {
 		return nil, err
 	}
+	fmt.Println(result.Expiration())
 	return result.PrivateClaims(), nil
 }
--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 
 require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.1.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/goccy/go-json v0.9.11 // indirect
@@ -27,6 +28,8 @@ require (
 	github.com/lestrrat-go/option v1.0.0 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.8.2 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.1 // indirect
 	github.com/xdg-go/stringprep v1.0.3 // indirect
@@ -34,6 +37,7 @@ require (
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+	golang.org/x/exp v0.0.0-20230321023759-10a507213a29 // indirect
 	golang.org/x/net v0.7.0 // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/text v0.8.0 // indirect
